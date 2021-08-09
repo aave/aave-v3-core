615ca0be8da2f80e9d5420f65af65a3954ca2786
miguelmtzinf <miguelmtz.mail@gmail.com>
2021-08-09 18:06:16 +0200

    feat: Add fix in WETH9Mocked

--- contracts/mocks/tokens/WETH9Mocked.sol
+++ contracts/mocks/tokens/WETH9Mocked.sol
@@ -5,8 +5,9 @@ import {WETH9} from '../dependencies/weth/WETH9.sol';
 
 contract WETH9Mocked is WETH9 {
   // Mint not backed by Ether: only for testing purposes
-  function mint(uint256 value) public {
+  function mint(uint256 value) public returns (bool) {
     balanceOf[msg.sender] += value;
     emit Transfer(address(0), msg.sender, value);
+    return true;
   }
 }
