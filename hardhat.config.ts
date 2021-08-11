ba35688384410fbefa738d56c4feacd9361329f0
Zer0dot <zer0dot.dev@gmail.com>
2021-08-11 10:57:55 -0400

    fix: update hardhat config and tsconfig to match changes

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -16,7 +16,7 @@ require('dotenv').config();
 
 import '@nomiclabs/hardhat-ethers';
 import '@nomiclabs/hardhat-waffle';
-import 'temp-hardhat-etherscan';
+import '@nomiclabs/hardhat-etherscan';
 import 'hardhat-gas-reporter';
 import 'hardhat-typechain';
 import '@tenderly/hardhat-tenderly';
