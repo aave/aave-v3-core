90c59fbf4b5ee309cceadc1d16fea89a924e1f4c
Lasse Herskind <16536249+LHerskind@users.noreply.github.com>
2021-08-20 12:33:24 +0200

    test: Add missing cases for `DepositLogic`
    - add case to `executeWithdraw()`
    - add cases to `finalizeTransfer()`

--- test-suites/atoken-transfer.spec.ts
+++ test-suites/atoken-transfer.spec.ts
@@ -5,6 +5,8 @@ import { ethers } from 'ethers';
 import { RateMode, ProtocolErrors } from '../helpers/types';
 import { makeSuite, TestEnv } from './helpers/make-suite';
 import AaveConfig from '../market-config';
+import { zeroAddress } from 'ethereumjs-util';
+import { evmRevert, evmSnapshot } from '../helpers/misc-utils';
 
 const AAVE_REFERRAL = AaveConfig.ProtocolGlobalParams.AaveReferral;
 
@@ -14,6 +16,117 @@ makeSuite('AToken: Transfer', (testEnv: TestEnv) => {
     INVALID_TO_BALANCE_AFTER_TRANSFER,
     VL_HEALTH_FACTOR_LOWER_THAN_LIQUIDATION_THRESHOLD,
   } = ProtocolErrors;
+
+  it('User 0 deposits 1000 DAI, transfers 1000 to user 0', async () => {
+    const { users, pool, dai, aDai } = testEnv;
+    const snap = await evmSnapshot();
+
+    await dai.connect(users[0].signer).mint(await convertToCurrencyDecimals(dai.address, '1000'));
+
+    await dai.connect(users[0].signer).approve(pool.address, APPROVAL_AMOUNT_POOL);
+
+    //user 1 deposits 1000 DAI
+    const amountDAItoDeposit = await convertToCurrencyDecimals(dai.address, '1000');
+
+    await pool
+      .connect(users[0].signer)
+      .deposit(dai.address, amountDAItoDeposit, users[0].address, '0');
+
+    await aDai.connect(users[0].signer).transfer(users[0].address, amountDAItoDeposit);
+
+    const name = await aDai.name();
+
+    expect(name).to.be.equal('Aave interest bearing DAI');
+
+    const fromBalance = await aDai.balanceOf(users[0].address);
+    const toBalance = await aDai.balanceOf(users[0].address);
+    expect(fromBalance.toString()).to.be.eq(toBalance.toString());
+
+    await evmRevert(snap);
+  });
+
+  it('User 0 deposits 1000 DAI, disable as collateral, transfers 1000 to user 1', async () => {
+    const { users, pool, dai, aDai } = testEnv;
+    const snap = await evmSnapshot();
+
+    await dai.connect(users[0].signer).mint(await convertToCurrencyDecimals(dai.address, '1000'));
+
+    await dai.connect(users[0].signer).approve(pool.address, APPROVAL_AMOUNT_POOL);
+
+    //user 1 deposits 1000 DAI
+    const amountDAItoDeposit = await convertToCurrencyDecimals(dai.address, '1000');
+
+    await pool
+      .connect(users[0].signer)
+      .deposit(dai.address, amountDAItoDeposit, users[0].address, '0');
+
+    await pool.connect(users[0].signer).setUserUseReserveAsCollateral(dai.address, false);
+
+    await aDai.connect(users[0].signer).transfer(users[1].address, amountDAItoDeposit);
+
+    const name = await aDai.name();
+
+    expect(name).to.be.equal('Aave interest bearing DAI');
+
+    const fromBalance = await aDai.balanceOf(users[0].address);
+    const toBalance = await aDai.balanceOf(users[1].address);
+    expect(fromBalance.toString()).to.be.equal('0', INVALID_FROM_BALANCE_AFTER_TRANSFER);
+    expect(toBalance.toString()).to.be.equal(
+      amountDAItoDeposit.toString(),
+      INVALID_TO_BALANCE_AFTER_TRANSFER
+    );
+
+    await evmRevert(snap);
+  });
+
+  it('User 0 deposits 1000 DAI, transfers 5 to user 1 x 2, then transfer 0 to user 1', async () => {
+    const { users, pool, dai, aDai } = testEnv;
+    const snap = await evmSnapshot();
+
+    await dai.connect(users[0].signer).mint(await convertToCurrencyDecimals(dai.address, '1000'));
+
+    await dai.connect(users[0].signer).approve(pool.address, APPROVAL_AMOUNT_POOL);
+
+    //user 1 deposits 1000 DAI
+    const amountDAItoDeposit = await convertToCurrencyDecimals(dai.address, '1000');
+    const amountDAItoTransfer = await convertToCurrencyDecimals(dai.address, '5');
+
+    await pool
+      .connect(users[0].signer)
+      .deposit(dai.address, amountDAItoDeposit, users[0].address, '0');
+
+    await aDai.connect(users[0].signer).transfer(users[1].address, amountDAItoTransfer);
+    expect(await aDai.balanceOf(users[0].address)).to.be.eq(
+      (await convertToCurrencyDecimals(dai.address, '995')).toString(),
+      INVALID_FROM_BALANCE_AFTER_TRANSFER
+    );
+    expect(await aDai.balanceOf(users[1].address)).to.be.eq(
+      (await convertToCurrencyDecimals(dai.address, '5')).toString(),
+      INVALID_TO_BALANCE_AFTER_TRANSFER
+    );
+
+    await aDai.connect(users[0].signer).transfer(users[1].address, amountDAItoTransfer);
+    expect(await aDai.balanceOf(users[0].address)).to.be.eq(
+      (await convertToCurrencyDecimals(dai.address, '990')).toString(),
+      INVALID_FROM_BALANCE_AFTER_TRANSFER
+    );
+    expect(await aDai.balanceOf(users[1].address)).to.be.eq(
+      (await convertToCurrencyDecimals(dai.address, '10')).toString(),
+      INVALID_TO_BALANCE_AFTER_TRANSFER
+    );
+
+    await aDai.connect(users[0].signer).transfer(users[1].address, 0);
+    expect(await aDai.balanceOf(users[0].address)).to.be.eq(
+      (await convertToCurrencyDecimals(dai.address, '990')).toString(),
+      INVALID_FROM_BALANCE_AFTER_TRANSFER
+    );
+    expect(await aDai.balanceOf(users[1].address)).to.be.eq(
+      (await convertToCurrencyDecimals(dai.address, '10')).toString(),
+      INVALID_TO_BALANCE_AFTER_TRANSFER
+    );
+
+    await evmRevert(snap);
+  });
 
   it('User 0 deposits 1000 DAI, transfers to user 1', async () => {
     const { users, pool, dai, aDai } = testEnv;
