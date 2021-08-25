f2f6c8427a3226c61aa0b4505f7b029d1e97b762
Zer0dot <zer0dot.dev@gmail.com>
2021-08-25 13:55:42 -0400

    misc: removed unused packages: `chai-bn`, `@nomiclabs/hardhat-waffle` and `@typechain/ethers-v4`

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -8,7 +8,6 @@ import { buildForkConfig } from './helper-hardhat-config';
 require('dotenv').config();
 
 import '@nomiclabs/hardhat-ethers';
-import '@nomiclabs/hardhat-waffle';
 import '@nomiclabs/hardhat-etherscan';
 import 'hardhat-gas-reporter';
 import 'hardhat-typechain';
