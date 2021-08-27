165828a947c1fcaa958171462df1212897be4325
miguelmtzinf <miguelmtz.mail@gmail.com>
2021-08-27 16:52:49 +0200

    refactor: Refactor BorrowRepaySameTx

--- test-suites/borrow-repay-same-tx.spec.ts
+++ test-suites/borrow-repay-same-tx.spec.ts
@@ -1,23 +1,15 @@
-import { TestEnv, makeSuite } from './helpers/make-suite';
-import { ProtocolErrors } from '../helpers/types';
-import {
-  MockBorrowRepayTest,
-  MockBorrowRepayTestFactory,
-  MintableERC20,
-  WETH9Mocked,
-} from '../types';
-import { BigNumber } from 'ethers';
-import { getFirstSigner } from '../helpers/contracts-getters';
 import { expect } from 'chai';
+import { ProtocolErrors } from '../helpers/types';
+import { MockBorrowRepayTest, MockBorrowRepayTestFactory } from '../types';
+import { getFirstSigner } from '../helpers/contracts-getters';
+import { TestEnv, makeSuite } from './helpers/make-suite';
 
-makeSuite('Borrow/repay in the same tx', (testEnv: TestEnv) => {
+makeSuite('Borrow/Repay in the same tx', (testEnv: TestEnv) => {
   const { VL_SAME_BLOCK_BORROW_REPAY } = ProtocolErrors;
-  const unitParse = async (token: WETH9Mocked | MintableERC20, nb: string) =>
-    BigNumber.from(nb).mul(BigNumber.from('10').pow((await token.decimals()) - 3));
 
   let testContract: MockBorrowRepayTest;
 
-  it('Deploys the test contract', async () => {
+  before(async () => {
     const { weth, dai, pool } = testEnv;
 
     testContract = await (
@@ -25,17 +17,15 @@ makeSuite('Borrow/repay in the same tx', (testEnv: TestEnv) => {
     ).deploy(pool.address, weth.address, dai.address);
   });
 
-  it('Executes a test borrow/repay in the same transaction at variable (revert expected)', async () => {
+  it('Executes a borrow/repay in the same transaction at variable and reverts', async () => {
     await expect(testContract.executeBorrowRepayVariable()).to.be.revertedWith(
-      VL_SAME_BLOCK_BORROW_REPAY,
-      'Borrow/repay in the same transaction did not revert as expected'
+      VL_SAME_BLOCK_BORROW_REPAY
     );
   });
 
-  it('Executes a test borrow/repay in the same transaction at stabke (revert expected)', async () => {
+  it('Executes a borrow/repay in the same transaction at stable and reverts', async () => {
     await expect(testContract.executeBorrowRepayStable()).to.be.revertedWith(
-      VL_SAME_BLOCK_BORROW_REPAY,
-      'Borrow/repay in the same transaction did not revert as expected'
+      VL_SAME_BLOCK_BORROW_REPAY
     );
   });
 });
