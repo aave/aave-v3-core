1d2bee89a7f575ed8c9b8e58506e2831e5273e11
Lasse Herskind <16536249+LHerskind@users.noreply.github.com>
2021-09-01 14:25:44 +0200

    refactor: Refactor `liquidation-edge.spec.ts`
    - Use `convertToCurrencyDecimals` when it makes sense instead of parseUnits

--- test-suites/liquidation-edge.spec.ts
+++ test-suites/liquidation-edge.spec.ts
@@ -3,6 +3,7 @@ import { MAX_UINT_AMOUNT } from '../helpers/constants';
 import { RateMode } from '../helpers/types';
 import { evmRevert, evmSnapshot } from '../helpers/misc-utils';
 import { makeSuite, TestEnv } from './helpers/make-suite';
+import { convertToCurrencyDecimals } from '../helpers/contracts-helpers';
 
 makeSuite('Pool Liquidation: Edge cases', (testEnv: TestEnv) => {
   let snap: string;
@@ -21,30 +22,49 @@ makeSuite('Pool Liquidation: Edge cases', (testEnv: TestEnv) => {
     const borrower = users[1];
 
     // Deposit 1000 dai
-    await dai.connect(depositor.signer).mint(utils.parseUnits('1000000', 18));
+    await dai
+      .connect(depositor.signer)
+      .mint(await convertToCurrencyDecimals(dai.address, '1000000'));
     await dai.connect(depositor.signer).approve(pool.address, MAX_UINT_AMOUNT);
     await pool
       .connect(depositor.signer)
-      .deposit(dai.address, utils.parseUnits('10000', 18), depositor.address, 0);
+      .deposit(
+        dai.address,
+        await convertToCurrencyDecimals(dai.address, '10000'),
+        depositor.address,
+        0
+      );
 
     // Deposit eth, borrow dai
-    await weth.connect(borrower.signer).mint(utils.parseUnits('1', 18));
+    await weth.connect(borrower.signer).mint(utils.parseEther('1'));
     await weth.connect(borrower.signer).approve(pool.address, MAX_UINT_AMOUNT);
     await pool
       .connect(borrower.signer)
-      .deposit(weth.address, utils.parseUnits('1', 18), borrower.address, 0);
+      .deposit(weth.address, utils.parseEther('1'), borrower.address, 0);
 
     await oracle.setAssetPrice(dai.address, utils.parseUnits('0.001', 18));
 
     // Borrow 500 dai stable
     await pool
       .connect(borrower.signer)
-      .borrow(dai.address, utils.parseUnits('500', 18), RateMode.Stable, 0, borrower.address);
+      .borrow(
+        dai.address,
+        await convertToCurrencyDecimals(dai.address, '500'),
+        RateMode.Stable,
+        0,
+        borrower.address
+      );
 
     // Borrow 200 dai variable
     await pool
       .connect(borrower.signer)
-      .borrow(dai.address, utils.parseUnits('200', 18), RateMode.Variable, 0, borrower.address);
+      .borrow(
+        dai.address,
+        await convertToCurrencyDecimals(dai.address, '200'),
+        RateMode.Variable,
+        0,
+        borrower.address
+      );
 
     await oracle.setAssetPrice(dai.address, utils.parseUnits('0.002', 18));
 
