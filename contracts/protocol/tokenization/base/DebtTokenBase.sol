467b6f462430f11778557cc3bdcc68b18cb9cd83
emilio <emilio@ethlend.io>
2021-09-02 12:56:11 +0200

    refactor: refactored delegation with signature

--- contracts/protocol/tokenization/base/DebtTokenBase.sol
+++ contracts/protocol/tokenization/base/DebtTokenBase.sol
@@ -21,6 +21,15 @@ abstract contract DebtTokenBase is
   ICreditDelegationToken
 {
   mapping(address => mapping(address => uint256)) internal _borrowAllowances;
+  bytes public constant EIP712_REVISION = bytes('1');
+  bytes32 internal constant EIP712_DOMAIN =
+    keccak256('EIP712Domain(string name,string version,uint256 chainId,address verifyingContract)');
+  bytes32 public constant DELEGATION_WITH_SIG_TYPEHASH =
+    keccak256(
+      'DelegationWithSig(address delegator,address delegatee,uint256 value,uint256 nonce,uint256 deadline)'
+    );
+  mapping(address => uint256) public _nonces;
+  bytes32 public DOMAIN_SEPARATOR;
 
   /**
    * @dev Only pool can call functions marked by this modifier
@@ -39,6 +48,51 @@ abstract contract DebtTokenBase is
    **/
   function approveDelegation(address delegatee, uint256 amount) external override {
     _approveDelegation(_msgSender(), delegatee, amount);
+  }
+
+  /**
+   * @dev implements the credit delegation with ERC712 signature
+   * @param delegator The delegator of the credit
+   * @param delegatee The delegatee that can use the credit
+   * @param value The amount to be delegated
+   * @param deadline The deadline timestamp, type(uint256).max for max deadline
+   * @param v Signature param
+   * @param s Signature param
+   * @param r Signature param
+   */
+  function delegationWithSig(
+    address delegator,
+    address delegatee,
+    uint256 value,
+    uint256 deadline,
+    uint8 v,
+    bytes32 r,
+    bytes32 s
+  ) external {
+    require(delegator != address(0), 'INVALID_DELEGATOR');
+    //solium-disable-next-line
+    require(block.timestamp <= deadline, 'INVALID_EXPIRATION');
+    uint256 currentValidNonce = _nonces[delegator];
+    bytes32 digest =
+      keccak256(
+        abi.encodePacked(
+          '\x19\x01',
+          DOMAIN_SEPARATOR,
+          keccak256(
+            abi.encode(
+              DELEGATION_WITH_SIG_TYPEHASH,
+              delegator,
+              delegatee,
+              value,
+              currentValidNonce,
+              deadline
+            )
+          )
+        )
+      );
+    require(delegator == ecrecover(digest, v, r, s), 'INVALID_SIGNATURE');
+    _nonces[delegator] = currentValidNonce + 1;
+    _approveDelegation(delegator, delegatee, value);
   }
 
   /**
@@ -131,8 +185,7 @@ abstract contract DebtTokenBase is
     address delegatee,
     uint256 amount
   ) internal {
-    uint256 newAllowance =
-      _borrowAllowances[delegator][delegatee] - amount;
+    uint256 newAllowance = _borrowAllowances[delegator][delegatee] - amount;
 
     _borrowAllowances[delegator][delegatee] = newAllowance;
 
