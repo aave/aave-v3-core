9d9d26b220f3d61fe35a616588c9a280575c254a
Lasse Herskind <16536249+LHerskind@users.noreply.github.com>
2021-09-07 12:50:29 +0200

    refactor: Refactor comments for `Helpers`

--- contracts/protocol/libraries/helpers/Helpers.sol
+++ contracts/protocol/libraries/helpers/Helpers.sol
@@ -13,7 +13,8 @@ library Helpers {
    * @dev Fetches the user current stable and variable debt balances
    * @param user The user address
    * @param reserve The reserve data object
-   * @return The stable and variable debt balance
+   * @return The stable debt balance
+   * @return The variable debt balance
    **/
   function getUserCurrentDebt(address user, DataTypes.ReserveData storage reserve)
     internal
