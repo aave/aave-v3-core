389156b67d64092db3383e26afacb8e71528c6da
Lasse Herskind <16536249+LHerskind@users.noreply.github.com>
2021-09-08 11:48:25 +0200

    refactor: Update dev/notice to better follow natspec in `IPoolAddressesProvider`

--- contracts/interfaces/IPoolAddressesProvider.sol
+++ contracts/interfaces/IPoolAddressesProvider.sol
@@ -3,10 +3,10 @@ pragma solidity 0.8.6;
 
 /**
  * @title PoolAddressesProvider contract
- * @dev Main registry of addresses part of or connected to the protocol, including permissioned roles
- * - Acting also as factory of proxies and admin of those, so with right to change its implementations
- * - Owned by the Aave Governance
  * @author Aave
+ * @notice Main registry of addresses part of or connected to the protocol, including permissioned roles
+ * @dev Acting also as factory of proxies and admin of those, so with right to change its implementations
+ *      Owned by the Aave Governance
  **/
 interface IPoolAddressesProvider {
   event MarketIdSet(string newMarketId);
@@ -20,30 +20,30 @@ interface IPoolAddressesProvider {
   event AddressSet(bytes32 id, address indexed newAddress, bool hasProxy);
 
   /**
-   * @dev Returns the id of the Aave market to which this contracts points to
+   * @notice Returns the id of the Aave market to which this contracts points to
    * @return The market id
    **/
   function getMarketId() external view returns (string memory);
 
   /**
-   * @dev Allows to set the market which this PoolAddressesProvider represents
+   * @notice Allows to set the market which this PoolAddressesProvider represents
    * @param marketId The market id
    */
   function setMarketId(string calldata marketId) external;
 
   /**
-   * @dev Sets an address for an id replacing the address saved in the addresses map
-   * IMPORTANT Use this function carefully, as it will do a hard replacement
+   * @notice Sets an address for an id replacing the address saved in the addresses map
+   * @dev IMPORTANT Use this function carefully, as it will do a hard replacement
    * @param id The id
    * @param newAddress The address to set
    */
   function setAddress(bytes32 id, address newAddress) external;
 
   /**
-   * @dev General function to update the implementation of a proxy registered with
+   * @notice General function to update the implementation of a proxy registered with
    * certain `id`. If there is no proxy registered, it will instantiate one and
    * set as implementation the `impl`
-   * IMPORTANT Use this function carefully, only for ids that don't have an explicit
+   * @dev IMPORTANT Use this function carefully, only for ids that don't have an explicit
    * setter function, in order to avoid unexpected consequences
    * @param id The id
    * @param impl The address of the new implementation
@@ -51,39 +51,39 @@ interface IPoolAddressesProvider {
   function setAddressAsProxy(bytes32 id, address impl) external;
 
   /**
-   * @dev Returns an address by id
+   * @notice Returns an address by id
    * @return The address
    */
   function getAddress(bytes32 id) external view returns (address);
 
   /**
-   * @dev Returns the address of the Pool proxy
+   * @notice Returns the address of the Pool proxy
    * @return The Pool proxy address
    **/
   function getPool() external view returns (address);
 
   /**
-   * @dev Updates the implementation of the Pool, or creates the proxy
+   * @notice Updates the implementation of the Pool, or creates the proxy
    * setting the new `pool` implementation on the first time calling it
    * @param pool The new Pool implementation
    **/
   function setPoolImpl(address pool) external;
 
   /**
-   * @dev Returns the address of the PoolConfigurator proxy
+   * @notice Returns the address of the PoolConfigurator proxy
    * @return The PoolConfigurator proxy address
    **/
   function getPoolConfigurator() external view returns (address);
 
   /**
-   * @dev Updates the implementation of the PoolConfigurator, or creates the proxy
+   * @notice Updates the implementation of the PoolConfigurator, or creates the proxy
    * setting the new `configurator` implementation on the first time calling it
    * @param configurator The new PoolConfigurator implementation
    **/
   function setPoolConfiguratorImpl(address configurator) external;
 
   /**
-   * @dev The functions below are getters/setters of addresses that are outside the context
+   * @notice The functions below are getters/setters of addresses that are outside the context
    * of the protocol hence the upgradable proxy pattern is not used
    **/
 
