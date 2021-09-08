c57f72d25eabfa6a635ac90d4c983acc4328b922
Lasse Herskind <16536249+LHerskind@users.noreply.github.com>
2021-09-08 11:53:53 +0200

    refactor: Update dev/notice to better follow natspec in `IPoolAddressesProviderRegistry`

--- contracts/interfaces/IPoolAddressesProviderRegistry.sol
+++ contracts/interfaces/IPoolAddressesProviderRegistry.sol
@@ -3,24 +3,24 @@ pragma solidity 0.8.6;
 
 /**
  * @title PoolAddressesProviderRegistry contract
- * @dev Main registry of PoolAddressesProvider of multiple Aave protocol's markets
+ * @author Aave
+ * @notice Main registry of PoolAddressesProvider of multiple Aave protocol's markets
  * - Used for indexing purposes of Aave protocol's markets
  * - The id assigned to a PoolAddressesProvider refers to the market it is connected with,
  *   for example with `0` for the Aave main market and `1` for the next created
- * @author Aave
  **/
 interface IPoolAddressesProviderRegistry {
   event AddressesProviderRegistered(address indexed newAddress);
   event AddressesProviderUnregistered(address indexed newAddress);
 
   /**
-   * @dev Returns the list of registered addresses provider
+   * @notice Returns the list of registered addresses provider
    * @return The list of addresses provider, potentially containing address(0) elements
    **/
   function getAddressesProvidersList() external view returns (address[] memory);
 
   /**
-   * @dev Returns the id on a registered PoolAddressesProvider
+   * @notice Returns the id on a registered PoolAddressesProvider
    * @return The id or 0 if the PoolAddressesProvider is not registered
    */
   function getAddressesProviderIdByAddress(address addressesProvider)
@@ -29,14 +29,14 @@ interface IPoolAddressesProviderRegistry {
     returns (uint256);
 
   /**
-   * @dev Registers an addresses provider
+   * @notice Registers an addresses provider
    * @param provider The address of the new PoolAddressesProvider
    * @param id The id for the new PoolAddressesProvider, referring to the market it belongs to
    **/
   function registerAddressesProvider(address provider, uint256 id) external;
 
   /**
-   * @dev Removes a PoolAddressesProvider from the list of registered addresses provider
+   * @notice Removes a PoolAddressesProvider from the list of registered addresses provider
    * @param provider The PoolAddressesProvider address
    **/
   function unregisterAddressesProvider(address provider) external;
