809a56257ed79593acdf2ae84bb7f5ab5997930e
Lasse Herskind <16536249+LHerskind@users.noreply.github.com>
2021-09-08 12:14:19 +0200

    refactor: Update dev/notice to better follow natspec in `IPriceOracle`

--- contracts/interfaces/IPriceOracle.sol
+++ contracts/interfaces/IPriceOracle.sol
@@ -2,18 +2,20 @@
 pragma solidity 0.8.6;
 
 /**
- * @title IPriceOracle interface
+ * @title IPriceOracle
+ * @author Aave
+ * @notice Defines the basic interface for an Price oracles.
  **/
 interface IPriceOracle {
   /**
-   * @dev Returns the asset price in the base currency
+   * @notice Returns the asset price in the base currency
    * @param asset The address of the asset
    * @return The price of the asset
    **/
   function getAssetPrice(address asset) external view returns (uint256);
 
   /**
-   * @dev Set the price of the asset
+   * @notice Set the price of the asset
    * @param asset The address of the asset
    * @param price The price of the asset
    **/
