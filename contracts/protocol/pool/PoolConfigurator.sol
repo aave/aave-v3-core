a82d3039653ec9574a6c9ae1637307fcfb1a63da
Lasse Herskind <16536249+LHerskind@users.noreply.github.com>
2021-10-06 13:10:14 +0200

    fix: Remove old call

--- contracts/protocol/pool/PoolConfigurator.sol
+++ contracts/protocol/pool/PoolConfigurator.sol
@@ -411,10 +411,6 @@ contract PoolConfigurator is VersionedInitializable, IPoolConfigurator {
   function _checkNoLiquidity(address asset) internal view {
     uint256 totalATokens = IPoolDataProvider(_addressesProvider.getPoolDataProvider())
       .getATokenTotalSupply(asset);
-    /*(, , uint256 totalATokens, , , , , , , , , ) = IPoolDataProvider(
-      _addressesProvider.getPoolDataProvider()
-    ).getReserveData(asset);*/
-
     require(totalATokens == 0, Errors.PC_RESERVE_LIQUIDITY_NOT_0);
   }
 
