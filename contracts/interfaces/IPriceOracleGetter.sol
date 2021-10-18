8af6d0aed5de659676db5b920267596454d8678d
sendra <polsendra@gmail.com>
2021-10-18 13:43:10 +0200

    fix: added public vars to interface for periphery usage

--- contracts/interfaces/IPriceOracleGetter.sol
+++ contracts/interfaces/IPriceOracleGetter.sol
@@ -8,6 +8,18 @@ pragma solidity 0.8.7;
  **/
 interface IPriceOracleGetter {
   /**
+   * @notice Returns the base currency address
+   * @return Returns the base currency address. MOCK_USD_ADDRESS if usd market
+   **/
+  function BASE_CURRENCY() external view returns (address);
+
+  /**
+   * @notice Returns the base currency unit
+   * @return Returns the base currency unit. 1 ether if eth market and 100000000 if usd market
+   **/
+  function BASE_CURRENCY_UNIT() external view returns (uint256);
+
+  /**
    * @notice Returns the asset price in the base currency
    * @param asset The address of the asset
    * @return The price of the asset
