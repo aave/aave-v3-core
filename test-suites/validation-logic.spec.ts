cdc27ee1ff8c952275f715f2ef514c5fc1a69bc5
Lasse Herskind <16536249+LHerskind@users.noreply.github.com>
2021-11-11 10:31:43 +0000

    test: Add additional for adding EMode with invalid parameters and update existing
    - Update existing test for `setAssetEModeCategory` and `setUserEMode` which used inconsistent EMode categories.
    - Add additional test for inconsistent EMode categories

--- test-suites/validation-logic.spec.ts
+++ test-suites/validation-logic.spec.ts
@@ -720,19 +720,11 @@ makeSuite('ValidationLogic: Edge cases', (testEnv: TestEnv) => {
     ).to.be.revertedWith(VL_RESERVE_PAUSED);
   });
 
-  it('validateSetUserEMode() with LT == 0 (reverts)', async () => {
+  it('validateSetUserEMode() to undefined emode category (revert expected)', async () => {
     const {
-      configurator,
-      poolAdmin,
       pool,
       users: [user],
     } = testEnv;
-
-    expect(
-      await configurator
-        .connect(poolAdmin.signer)
-        .setEModeCategory('101', '9800', '0', '10100', ethers.constants.AddressZero, 'INCONSISTENT')
-    );
 
     await expect(pool.connect(user.signer).setUserEMode(101)).to.be.revertedWith(
       VL_INCONSISTENT_EMODE_CATEGORY
