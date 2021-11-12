942c93222164b9870d831ed1ecdf3a66439fca3e
kartojal <canillas.mail@gmail.com>
2021-11-12 14:53:58 +0100

    fix: remove unneeded if at __setup.spect.ts

--- test-suites/__setup.spec.ts
+++ test-suites/__setup.spec.ts
@@ -2,19 +2,10 @@ import rawBRE from 'hardhat';
 import { initializeMakeSuite } from './helpers/make-suite';
 
 before(async () => {
-  const FORK = process.env.FORK;
-
   process.env.MARKET_NAME = 'Aave';
 
-  if (FORK) {
-    //await rawBRE.run('aave:mainnet');
-    await rawBRE.deployments.fixture(['market']);
-  } else {
-    console.log('-> Deploying market...');
-    //await buildTestEnv(deployer, secondaryWallet);
-    // Deploy Aave Market as fixture
-    await rawBRE.deployments.fixture(['market']);
-  }
+  await rawBRE.deployments.fixture(['market']);
+
   console.log('-> Deployed market');
 
   console.log('-> Initializing test enviroment');
