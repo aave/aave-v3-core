1f02c090b7922f9a08ad6e5bc29d350b2736060b
Lasse Herskind <16536249+LHerskind@users.noreply.github.com>
2021-12-02 11:02:48 +0000

    fix: Fix typo in `IAaveOracle` natspec

--- contracts/interfaces/IAaveOracle.sol
+++ contracts/interfaces/IAaveOracle.sol
@@ -52,7 +52,7 @@ interface IAaveOracle is IPriceOracleGetter {
   /**
    * @notice Returns a list of prices from a list of assets addresses
    * @param assets The list of assets addresses
-   * @return The prices of te given assets
+   * @return The prices of the given assets
    */
   function getAssetsPrices(address[] calldata assets) external view returns (uint256[] memory);
 
