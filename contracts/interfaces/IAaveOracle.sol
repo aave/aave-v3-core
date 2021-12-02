ed03e3097aaae33c202d042bd6bb0f1d9a03eb26
miguelmtzinf <miguelmtz.mail@gmail.com>
2021-12-02 11:15:59 +0100

    fix: Add IAaveOracle interface

---
+++ contracts/interfaces/IAaveOracle.sol
@@ -0,0 +1,71 @@
+// SPDX-License-Identifier: agpl-3.0
+pragma solidity 0.8.10;
+
+import {IPriceOracleGetter} from './IPriceOracleGetter.sol';
+import {IPoolAddressesProvider} from './IPoolAddressesProvider.sol';
+
+/**
+ * @title IAaveOracle
+ * @author Aave
+ * @notice Defines the basic interface for the Aave Oracle
+ */
+interface IAaveOracle is IPriceOracleGetter {
+  /**
+   * @notice Emitted after the base currency is set
+   * @param baseCurrency The base currency of used for price quotes
+   * @param baseCurrencyUnit The unit of the base currency
+   */
+  event BaseCurrencySet(address indexed baseCurrency, uint256 baseCurrencyUnit);
+
+  /**
+   * @notice Emitted after the price source of an asset is updated
+   * @param asset The address of the asset
+   * @param source The price source of the asset
+   */
+  event AssetSourceUpdated(address indexed asset, address indexed source);
+
+  /**
+   * @notice Emitted after the address of fallback oracle is updated
+   * @param fallbackOracle The address of the fallback oracle
+   */
+  event FallbackOracleUpdated(address indexed fallbackOracle);
+
+  /**
+   * @notice Returns the PoolAddressesProvider
+   * @return The address of the PoolAddressesProvider contract
+   */
+  function ADDRESSES_PROVIDER() external view returns (IPoolAddressesProvider);
+
+  /**
+   * @notice Sets or replaces price sources of assets
+   * @param assets The addresses of the assets
+   * @param sources The addresses of the price sources
+   */
+  function setAssetSources(address[] calldata assets, address[] calldata sources) external;
+
+  /**
+   * @notice Sets the fallback oracle
+   * @param fallbackOracle The address of the fallback oracle
+   */
+  function setFallbackOracle(address fallbackOracle) external;
+
+  /**
+   * @notice Returns a list of prices from a list of assets addresses
+   * @param assets The list of assets addresses
+   * @return The prices of te given assets
+   */
+  function getAssetsPrices(address[] calldata assets) external view returns (uint256[] memory);
+
+  /**
+   * @notice Returns the address of the source for an asset address
+   * @param asset The address of the asset
+   * @return The address of the source
+   */
+  function getSourceOfAsset(address asset) external view returns (address);
+
+  /**
+   * @notice Returns the address of the fallback oracle
+   * @return The address of the fallback oracle
+   */
+  function getFallbackOracle() external view returns (address);
+}
