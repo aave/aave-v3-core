60a03e38157ed8c64fab8efd073d72e183f17a7b
miguelmtzinf <miguelmtz.mail@gmail.com>
2021-12-03 13:44:40 +0100

    fix: Clean code style of mocks

--- contracts/mocks/oracle/CLAggregators/MockAggregator.sol
+++ contracts/mocks/oracle/CLAggregators/MockAggregator.sol
@@ -6,9 +6,9 @@ contract MockAggregator {
 
   event AnswerUpdated(int256 indexed current, uint256 indexed roundId, uint256 timestamp);
 
-  constructor(int256 _initialAnswer) {
-    _latestAnswer = _initialAnswer;
-    emit AnswerUpdated(_initialAnswer, 0, block.timestamp);
+  constructor(int256 initialAnswer) {
+    _latestAnswer = initialAnswer;
+    emit AnswerUpdated(initialAnswer, 0, block.timestamp);
   }
 
   function latestAnswer() external view returns (int256) {
