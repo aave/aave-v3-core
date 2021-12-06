71be75eb0362c214b4a28292455d9faaf473d24a
Lasse Herskind <16536249+LHerskind@users.noreply.github.com>
2021-12-06 14:14:02 +0000

    fix: Fix BridgeLogic natspec

--- contracts/protocol/libraries/logic/BridgeLogic.sol
+++ contracts/protocol/libraries/logic/BridgeLogic.sol
@@ -23,6 +23,7 @@ library BridgeLogic {
   using PercentageMath for uint256;
   using SafeERC20 for IERC20;
 
+  // See `IPool` for descriptions
   event ReserveUsedAsCollateralEnabled(address indexed reserve, address indexed user);
   event MintUnbacked(
     address indexed reserve,
@@ -36,8 +37,12 @@ library BridgeLogic {
   /**
    * @notice Mint unbacked aTokens to a user and updates the unbacked for the reserve.
    * @dev Essentially a supply without transferring the underlying.
+   * @dev Emits the `MintUnbacked` event
+   * @dev Emits the `ReserveUsedAsCollateralEnabled` if asset is set as collateral
+   * @param reserves The state of all the reserves
+   * @param reservesList The list of the addresses of all the active reserves
    * @param reserve The reserve to mint to
-   * @param userConfig The user configuration to update
+   * @param userConfig The user configuration mapping that tracks the supplied/borrowed assets
    * @param asset The address of the asset
    * @param amount The amount to mint
    * @param onBehalfOf The address that will receive the aTokens
@@ -90,6 +95,7 @@ library BridgeLogic {
 
   /**
    * @notice Back the current unbacked with `amount` and pay `fee`.
+   * @dev Emits the `BackUnbacked` event
    * @param reserve The reserve to back unbacked for
    * @param asset The address of the underlying asset to repay
    * @param amount The amount to back
