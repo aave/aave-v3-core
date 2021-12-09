0c131367e8d1af5f4067753e62c5be3fd51336d5
miguelmtzinf <miguelmtz.mail@gmail.com>
2021-12-09 12:25:02 +0100

    test: Add case for listing addresses provider after removal

--- test-suites/addresses-provider-registry.spec.ts
+++ test-suites/addresses-provider-registry.spec.ts
@@ -2,11 +2,13 @@ import { expect } from 'chai';
 import { ZERO_ADDRESS } from '../helpers/constants';
 import { ProtocolErrors } from '../helpers/types';
 import { TestEnv, makeSuite } from './helpers/make-suite';
+import { ONE_ADDRESS } from '@aave/deploy-v3';
 
 makeSuite('AddressesProviderRegistry', (testEnv: TestEnv) => {
+  const NEW_ADDRESES_PROVIDER_ADDRESS = ONE_ADDRESS;
   const NEW_ADDRESSES_PROVIDER_ID = 2;
 
-  it('Checks the addresses provider is added to the registry', async () => {
+  it.only('Checks the addresses provider is added to the registry', async () => {
     const { addressesProvider, registry } = testEnv;
 
     const providers = await registry.getAddressesProvidersList();
@@ -18,57 +20,70 @@ makeSuite('AddressesProviderRegistry', (testEnv: TestEnv) => {
     );
   });
 
-  it('Tries to register an addresses provider with id 0 (revert expected)', async () => {
-    const { users, registry } = testEnv;
+  it.only('Tries to register an addresses provider with id 0 (revert expected)', async () => {
+    const { registry } = testEnv;
     const { PAPR_INVALID_ADDRESSES_PROVIDER_ID } = ProtocolErrors;
 
-    await expect(registry.registerAddressesProvider(users[2].address, '0')).to.be.revertedWith(
-      PAPR_INVALID_ADDRESSES_PROVIDER_ID
-    );
+    await expect(
+      registry.registerAddressesProvider(NEW_ADDRESES_PROVIDER_ADDRESS, '0')
+    ).to.be.revertedWith(PAPR_INVALID_ADDRESSES_PROVIDER_ID);
   });
 
-  it('Registers a new mock addresses provider', async () => {
-    const { users, registry } = testEnv;
+  it.only('Registers a new mock addresses provider', async () => {
+    const { registry } = testEnv;
 
-    // Simulating an addresses provider using the users[1] wallet address
-    expect(await registry.registerAddressesProvider(users[1].address, NEW_ADDRESSES_PROVIDER_ID))
+    const providersBefore = await registry.getAddressesProvidersList();
+
+    expect(
+      await registry.registerAddressesProvider(
+        NEW_ADDRESES_PROVIDER_ADDRESS,
+        NEW_ADDRESSES_PROVIDER_ID
+      )
+    )
       .to.emit(registry, 'AddressesProviderRegistered')
-      .withArgs(users[1].address);
+      .withArgs(NEW_ADDRESES_PROVIDER_ADDRESS);
 
-    const providers = await registry.getAddressesProvidersList();
+    expect(await registry.getAddressesProviderIdByAddress(NEW_ADDRESES_PROVIDER_ADDRESS)).to.be.eq(
+      NEW_ADDRESSES_PROVIDER_ID
+    );
 
-    expect(providers.length).to.be.equal(
-      NEW_ADDRESSES_PROVIDER_ID,
+    const providersAfter = await registry.getAddressesProvidersList();
+    expect(providersAfter.length).to.be.equal(
+      providersBefore.length + 1,
       'Invalid length of the addresses providers list'
     );
-    expect(providers[1].toString()).to.be.equal(
-      users[1].address,
+    expect(providersAfter[1].toString()).to.be.equal(
+      NEW_ADDRESES_PROVIDER_ADDRESS,
       'Invalid addresses provider added to the list'
     );
   });
 
-  it('Removes the mock addresses provider', async () => {
+  it.only('Removes the mock addresses provider', async () => {
     const { users, registry, addressesProvider } = testEnv;
 
-    const id = await registry.getAddressesProviderIdByAddress(users[1].address);
+    const providersBefore = await registry.getAddressesProvidersList();
 
-    expect(id).to.be.equal(NEW_ADDRESSES_PROVIDER_ID, 'Invalid isRegistered return value');
+    expect(
+      await registry.getAddressesProviderIdByAddress(NEW_ADDRESES_PROVIDER_ADDRESS)
+    ).to.be.equal(NEW_ADDRESSES_PROVIDER_ID);
 
-    expect(await registry.unregisterAddressesProvider(users[1].address))
+    expect(await registry.unregisterAddressesProvider(NEW_ADDRESES_PROVIDER_ADDRESS))
       .to.emit(registry, 'AddressesProviderUnregistered')
-      .withArgs(users[1].address);
+      .withArgs(NEW_ADDRESES_PROVIDER_ADDRESS);
 
-    const providers = await registry.getAddressesProvidersList();
+    const providersAfter = await registry.getAddressesProvidersList();
 
-    expect(providers.length).to.be.equal(2, 'Invalid length of the addresses providers list');
-    expect(providers[0].toString()).to.be.equal(
+    expect(providersAfter.length).to.be.equal(
+      providersBefore.length - 1,
+      'Invalid length of the addresses providers list'
+    );
+    expect(providersAfter[0].toString()).to.be.equal(
       addressesProvider.address,
       'Invalid addresses provider added to the list'
     );
-    expect(providers[1].toString()).to.be.equal(ZERO_ADDRESS, 'Invalid addresses');
   });
 
-  it('Tries to remove a unregistered addressesProvider (revert expected)', async () => {
+  it.only('Tries to remove an unregistered addressesProvider (revert expected)', async () => {
     const { PAPR_PROVIDER_NOT_REGISTERED } = ProtocolErrors;
 
     const { users, registry } = testEnv;
@@ -78,38 +93,31 @@ makeSuite('AddressesProviderRegistry', (testEnv: TestEnv) => {
     );
   });
 
-  it('Tries to remove a unregistered addressesProvider (revert expected)', async () => {
-    const { PAPR_PROVIDER_NOT_REGISTERED } = ProtocolErrors;
-
-    const { users, registry } = testEnv;
-
-    await expect(registry.unregisterAddressesProvider(users[2].address)).to.be.revertedWith(
-      PAPR_PROVIDER_NOT_REGISTERED
-    );
-  });
-
-  it('Tries to add an already added addressesProvider with a different id. Should overwrite the previous id', async () => {
+  it.only('Add an already added addressesProvider with a different id, overwriting the previous id', async () => {
     const { registry, addressesProvider } = testEnv;
 
     const oldId = await registry.getAddressesProviderIdByAddress(addressesProvider.address);
 
+    const providersBefore = await registry.getAddressesProvidersList();
     expect(
       await registry.registerAddressesProvider(addressesProvider.address, NEW_ADDRESSES_PROVIDER_ID)
     )
       .to.emit(registry, 'AddressesProviderRegistered')
       .withArgs(addressesProvider.address);
 
-    const providers = await registry.getAddressesProvidersList();
+    const providersAfter = await registry.getAddressesProvidersList();
 
     expect(await registry.getAddressesProviderIdByAddress(addressesProvider.address)).to.be.not.eq(
       oldId
     );
 
-    expect(providers.length).to.be.equal(2, 'Invalid length of the addresses providers list');
-    expect(providers[0].toString()).to.be.equal(
+    expect(providersAfter.length).to.be.equal(
+      providersBefore.length,
+      'Invalid length of the addresses providers list'
+    );
+    expect(providersAfter[0].toString()).to.be.equal(
       addressesProvider.address,
       'Invalid addresses provider added to the list'
     );
-    expect(providers[1].toString()).to.be.equal(ZERO_ADDRESS, 'Invalid addresses');
   });
 });
