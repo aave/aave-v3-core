5199283c5ac4e93260ad416920fb28aa8896ce5a
Steven Valeri <steven@aave.com>
2021-12-22 12:24:06 -0500

    fix: rename admin variable

--- contracts/protocol/libraries/aave-upgradeability/BaseImmutableAdminUpgradeabilityProxy.sol
+++ contracts/protocol/libraries/aave-upgradeability/BaseImmutableAdminUpgradeabilityProxy.sol
@@ -17,11 +17,11 @@ contract BaseImmutableAdminUpgradeabilityProxy is BaseUpgradeabilityProxy {
   address internal immutable _admin;
 
   constructor(address admin) {
-    ADMIN = admin;
+    _admin = admin;
   }
 
   modifier ifAdmin() {
-    if (msg.sender == ADMIN) {
+    if (msg.sender == _admin) {
       _;
     } else {
       _fallback();
@@ -33,7 +33,7 @@ contract BaseImmutableAdminUpgradeabilityProxy is BaseUpgradeabilityProxy {
    * @return The address of the proxy admin.
    */
   function admin() external ifAdmin returns (address) {
-    return ADMIN;
+    return _admin;
   }
 
   /**
@@ -76,7 +76,7 @@ contract BaseImmutableAdminUpgradeabilityProxy is BaseUpgradeabilityProxy {
    * @notice Only fall back when the sender is not the admin.
    */
   function _willFallback() internal virtual override {
-    require(msg.sender != ADMIN, 'Cannot call fallback function from the proxy admin');
+    require(msg.sender != _admin, 'Cannot call fallback function from the proxy admin');
     super._willFallback();
   }
 }
