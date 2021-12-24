592b21d831b0dc2a6e3e998701ea4e3a4b13c401
miguelmtzinf <miguelmtz.mail@gmail.com>
2021-12-24 12:36:34 +0100

    fix: Improve readability of initial values of variables

--- contracts/protocol/pool/Pool.sol
+++ contracts/protocol/pool/Pool.sol
@@ -94,8 +94,8 @@ contract Pool is VersionedInitializable, IPool, PoolStorage {
    **/
   function initialize(IPoolAddressesProvider provider) external initializer {
     require(provider == ADDRESSES_PROVIDER, Errors.PC_INVALID_CONFIGURATION);
-    _maxStableRateBorrowSizePercent = 2500;
-    _flashLoanPremiumTotal = 9;
+    _maxStableRateBorrowSizePercent = 0.25e4;
+    _flashLoanPremiumTotal = 0.0009e4;
     _flashLoanPremiumToProtocol = 0;
   }
 
