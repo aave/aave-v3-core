4568b05751ebe95a0e2c339d9fb86618a378d4c4
The3D <frangellaemilio@gmail.com>
2022-01-26 01:53:05 +0100

    refactor: removed import

--- contracts/protocol/libraries/logic/CalldataLogic.sol
+++ contracts/protocol/libraries/logic/CalldataLogic.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: BUSL-1.1
 pragma solidity 0.8.10;
 
-import {BytesLib} from '../../../dependencies/consensys/BytesLib.sol';
 import {DataTypes} from '../types/DataTypes.sol';
 
 /**
