4391fd4c8f29fb81c966fd4510df89d0acdb8404
miguelmtzinf <miguelmtz.mail@gmail.com>
2022-03-15 12:54:18 +0100

    fix: Fix docstrings of ISequencerOracle

--- contracts/interfaces/ISequencerOracle.sol
+++ contracts/interfaces/ISequencerOracle.sol
@@ -9,16 +9,12 @@ pragma solidity 0.8.10;
 interface ISequencerOracle {
   /**
    * @notice Returns the health status of the sequencer.
-   * @return roundId is the round ID from the aggregator for which the data was
-   * retrieved combined with a phase to ensure that round IDs get larger as
-   * time moves forward.   
-   * @return answer Is the answer for the latest round. The answer coming from the feed can only be 0 or 1.
-            0 - Sequencer is up
-            1 - Sequencer is down
-   * @return startedAt Is the timestamp when the round was started.
-   * @return updatedAt Is the block timestamp of the block in which the answer was updated on L1 
-   * @return answeredInRound is the round ID of the round in which the answer
-   * was computed.
+   * @return roundId The round ID from the aggregator for which the data was retrieved combined with a phase to ensure
+   * that round IDs get larger as time moves forward.
+   * @return answer The answer for the latest round: 0 if the sequencer is up, 1 if it is down.
+   * @return startedAt The timestamp when the round was started.
+   * @return updatedAt The timestamp of the block in which the answer was updated on L1.
+   * @return answeredInRound The round ID of the round in which the answer was computed.
    */
   function latestRoundData()
     external
