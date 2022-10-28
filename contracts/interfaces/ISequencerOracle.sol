0457e7176c89f91700e0aa63691dd0d9580b77db
miguelmtzinf <miguelmtz.mail@gmail.com>
2022-10-28 12:33:45 +0200

    feat: updated price oracle sentinel interface

--- contracts/interfaces/ISequencerOracle.sol
+++ contracts/interfaces/ISequencerOracle.sol
@@ -9,8 +9,21 @@ pragma solidity 0.8.10;
 interface ISequencerOracle {
   /**
    * @notice Returns the health status of the sequencer.
-   * @return True if the sequencer is down, false otherwise
-   * @return The timestamp of last time the sequencer got up
+   * @return roundId The round ID from the aggregator for which the data was retrieved combined with a phase to ensure
+   * that round IDs get larger as time moves forward.
+   * @return answer The answer for the latest round: 0 if the sequencer is up, 1 if it is down.
+   * @return startedAt The timestamp when the round was started.
+   * @return updatedAt The timestamp of the block in which the answer was updated on L1.
+   * @return answeredInRound The round ID of the round in which the answer was computed.
    */
-  function latestAnswer() external view returns (bool, uint256);
+  function latestRoundData()
+    external
+    view
+    returns (
+      uint80 roundId,
+      int256 answer,
+      uint256 startedAt,
+      uint256 updatedAt,
+      uint80 answeredInRound
+    );
 }
