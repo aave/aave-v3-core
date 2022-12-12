2b1354b446f7de4b813fe2c2bb6d56c4be5b3505
miguelmtz <36620902+miguelmtzinf@users.noreply.github.com>
2022-12-12 13:49:56 +0100

    Soften solidity version of Datatypes libraries (#762)
    * fix: Soften solidity version of DataTypes libraries
    
    * "Soft" Solidity to ^0.8.0 on libraries/math, libraries/configuration and libraries/helpers
    
    Co-authored-by: eboado <ebdmrr@gmail.com>

--- contracts/protocol/libraries/configuration/ReserveConfiguration.sol
+++ contracts/protocol/libraries/configuration/ReserveConfiguration.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: BUSL-1.1
-pragma solidity 0.8.10;
+pragma solidity ^0.8.0;
 
 import {Errors} from '../helpers/Errors.sol';
 import {DataTypes} from '../types/DataTypes.sol';
