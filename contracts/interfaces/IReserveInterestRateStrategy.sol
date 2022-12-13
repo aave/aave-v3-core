dd15e237034d26007711d9297b4670f5d68ac719
Lukas <lukasstrassel@googlemail.com>
2022-12-13 18:02:27 +0100

    fix: Complete interfaces of IReserveInterestRateStrategy and IPoolDataProvider (#763)
    * feat: complete interface & use inheritdoc
    
    * Added missing functions on IReserveInterestRateStrategy and inheritdoc on DefaultInterestRateStrategy
    
    * fix: added missing overrides, docs for constructor and added ADDRESSES_PROVIDER to interface
    
    * fix: sepparated interest rate strategy interface into default and rate interfaces
    
    * fix: added visibility to false rule
    
    * fix: minor fixes
    
    * fix: fixed natspec. Fixed import order
    
    * Update contracts/interfaces/IDefaultInterestRateStrategy.sol
    
    * Apply suggestions from code review
    
    * Apply suggestions from code review
    
    * fix: fixed correct import order
    
    Co-authored-by: eboado <ebdmrr@gmail.com>
    Co-authored-by: sendra <polsendra@gmail.com>
    Co-authored-by: sendra <sendra@users.noreply.github.com>
    Co-authored-by: miguelmtz <36620902+miguelmtzinf@users.noreply.github.com>

--- contracts/interfaces/IReserveInterestRateStrategy.sol
+++ contracts/interfaces/IReserveInterestRateStrategy.sol
@@ -10,24 +10,12 @@ import {DataTypes} from '../protocol/libraries/types/DataTypes.sol';
  */
 interface IReserveInterestRateStrategy {
   /**
-   * @notice Returns the base variable borrow rate
-   * @return The base variable borrow rate, expressed in ray
-   **/
-  function getBaseVariableBorrowRate() external view returns (uint256);
-
-  /**
-   * @notice Returns the maximum variable borrow rate
-   * @return The maximum variable borrow rate, expressed in ray
-   **/
-  function getMaxVariableBorrowRate() external view returns (uint256);
-
-  /**
    * @notice Calculates the interest rates depending on the reserve's state and configurations
    * @param params The parameters needed to calculate interest rates
    * @return liquidityRate The liquidity rate expressed in rays
    * @return stableBorrowRate The stable borrow rate expressed in rays
    * @return variableBorrowRate The variable borrow rate expressed in rays
-   **/
+   */
   function calculateInterestRates(DataTypes.CalculateInterestRatesParams memory params)
     external
     view
