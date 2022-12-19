428e2582a3b78624204d02ec2934bad77176c57d
Ernesto Boado <ebdmrr@gmail.com>
2022-12-19 11:07:51 +0100

    Clarify inconsistency between `getReserveNormalizedVariableDebt()` and `_updateIndexes` (#754) (#768)
    * Added clarification on getReserveNormalizedVariableDebt() in IPool
    * Updated docs on getReserveNormalizedVariableDebt IPool

--- contracts/interfaces/IPool.sol
+++ contracts/interfaces/IPool.sol
@@ -571,6 +571,13 @@ interface IPool {
 
   /**
    * @notice Returns the normalized variable debt per unit of asset
+   * @dev WARNING: This function is intended to be used primarily by the protocol itself to get a
+   * "dynamic" variable index based on time, current stored index and virtual rate at the current
+   * moment (approx. a borrower would get if opening a position). This means that is always used in
+   * combination with variable debt supply/balances.
+   * If using this function externally, consider that is possible to have an increasing normalized
+   * variable debt that is not equivalent to how the variable debt index would be updated in storage
+   * (e.g. only updates with non-zero variable debt supply)
    * @param asset The address of the underlying asset of the reserve
    * @return The reserve normalized variable debt
    */
