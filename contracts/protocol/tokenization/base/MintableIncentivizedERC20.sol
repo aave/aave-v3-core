8e4d0cd598914df55bc53e150ee7eaabd0957455
miguelmtzinf <miguelmtz.mail@gmail.com>
2023-01-10 13:27:44 +0100

    fix: Add BUSL to tokenization contracts

--- contracts/protocol/tokenization/base/MintableIncentivizedERC20.sol
+++ contracts/protocol/tokenization/base/MintableIncentivizedERC20.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: AGPL-3.0
+// SPDX-License-Identifier: BUSL-1.1
 pragma solidity 0.8.10;
 
 import {IAaveIncentivesController} from '../../../interfaces/IAaveIncentivesController.sol';
