35c4e55a89bbc686949a33a9bcb6c07f3d39e7f6
Michael M <MichaelM@certora.com>
2023-01-25 11:32:12 +0200

    fixing linter formatting and pulling changes from master

--- Certora/certora/scripts/verifySanityWithLinksOptimisticLoops.sh
+++
@@ -1,6 +0,0 @@
-certoraRun contracts/protocol/tokenization/AToken.sol contracts/protocol/pool/Pool.sol \
-  --verify AToken:specs/sanity.spec \
-  --link AToken:_pool=Pool \
-  --settings -t=300 \
-  --optimistic_loop \
-  --staging --msg "Aave AToken Sanity optimistic loops and link" 
