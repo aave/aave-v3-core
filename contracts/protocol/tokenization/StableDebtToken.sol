70512f930fe28848bd72c4703e1d968d35839cf4
Lukas <lukasstrassel@googlemail.com>
2023-01-31 22:26:43 +0100

    fix: bump stable token as well

--- contracts/protocol/tokenization/StableDebtToken.sol
+++ contracts/protocol/tokenization/StableDebtToken.sol
@@ -26,7 +26,7 @@ contract StableDebtToken is DebtTokenBase, IncentivizedERC20, IStableDebtToken {
   using WadRayMath for uint256;
   using SafeCast for uint256;
 
-  uint256 public constant DEBT_TOKEN_REVISION = 0x1;
+  uint256 public constant DEBT_TOKEN_REVISION = 0x2;
 
   // Map of users address and the timestamp of their last update (userAddress => lastUpdateTimestamp)
   mapping(address => uint40) internal _timestamps;
