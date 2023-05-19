2085bd29ad87d074510a38ad6fb48408e7aa27f3
WardenJakx <114708157+WardenJakx@users.noreply.github.com>
2023-05-18 22:48:27 -0400

    Fix pragma in FlashLoanSimpleReceiverBase.sol
    Typo in pragma only allowing version 0.8.10

--- contracts/flashloan/base/FlashLoanSimpleReceiverBase.sol
+++ contracts/flashloan/base/FlashLoanSimpleReceiverBase.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: AGPL-3.0
-pragma solidity 0.8.10;
+pragma solidity ^0.8.10;
 
 import {IFlashLoanSimpleReceiver} from '../interfaces/IFlashLoanSimpleReceiver.sol';
 import {IPoolAddressesProvider} from '../../interfaces/IPoolAddressesProvider.sol';
