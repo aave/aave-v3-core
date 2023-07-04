364a779efd9a7e42ec1882156d95a62f22ea3fc4
miguelmtz <36620902+miguelmtzinf@users.noreply.github.com>
2023-07-04 12:44:27 +0200

    fix: Soften solidity version of FlashLoanBase contracts (#861)

--- contracts/flashloan/base/FlashLoanReceiverBase.sol
+++ contracts/flashloan/base/FlashLoanReceiverBase.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: AGPL-3.0
-pragma solidity 0.8.10;
+pragma solidity ^0.8.10;
 
 import {IFlashLoanReceiver} from '../interfaces/IFlashLoanReceiver.sol';
 import {IPoolAddressesProvider} from '../../interfaces/IPoolAddressesProvider.sol';
